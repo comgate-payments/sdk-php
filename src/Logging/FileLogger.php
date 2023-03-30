721bd7caf66e8270929055a37c1a1f20798522c6
Tomas Novotny <tomas.novotny@comgate.cz>
2023-03-30 08:43:23 +0200

    psr log compatibility

--- src/Logging/FileLogger.php
+++ src/Logging/FileLogger.php
@@ -4,6 +4,7 @@ namespace Comgate\SDK\Logging;
 
 use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
+use Stringable;
 
 class FileLogger implements LoggerInterface
 {
@@ -26,42 +27,66 @@ class FileLogger implements LoggerInterface
 		file_put_contents($this->file, $message . "\n", FILE_APPEND);
 	}
 
-	public function emergency($message, array $context = array())
+	/**
+	 * @param mixed|string|Stringable $param
+	 */
+	public function emergency($message, array $context = []): void
 	{
 		$this->log(LogLevel::EMERGENCY, $message, $context);
 	}
 
-	public function alert($message, array $context = array())
+	/**
+	 * @param mixed|string|Stringable $param
+	 */
+	public function alert($message, array $context = []): void
 	{
 		$this->log(LogLevel::ALERT, $message, $context);
 	}
 
-	public function critical($message, array $context = array())
+	/**
+	 * @param mixed|string|Stringable $param
+	 */
+	public function critical($message, array $context = []): void
 	{
 		$this->log(LogLevel::CRITICAL, $message, $context);
 	}
 
-	public function error($message, array $context = array())
+	/**
+	 * @param mixed|string|Stringable $param
+	 */
+	public function error($message, array $context = []): void
 	{
 		$this->log(LogLevel::ERROR, $message, $context);
 	}
 
-	public function warning($message, array $context = array())
+	/**
+	 * @param mixed|string|Stringable $param
+	 */
+	public function warning($message, array $context = []): void
 	{
 		$this->log(LogLevel::WARNING, $message, $context);
 	}
 
-	public function notice($message, array $context = array())
+	/**
+	 * @param mixed|string|Stringable $param
+	 */
+	public function notice($message, array $context = []): void
 	{
 		$this->log(LogLevel::NOTICE, $message, $context);
 	}
 
-	public function info($message, array $context = array())
+	/**
+	 * @param mixed|string|Stringable $param
+	 */
+	public function info($message, array $context = []): void
 	{
 		$this->log(LogLevel::INFO, $message, $context);
 	}
 
-	public function debug($message, array $context = array())
+	/**
+	 * @param mixed|string|Stringable $param
+	 */
+	public function debug($message, array $context = []): void
 	{
 		$this->log(LogLevel::DEBUG, $message, $context);
 	}
--- src/Logging/StdoutLogger.php
+++ src/Logging/StdoutLogger.php
@@ -4,6 +4,7 @@ namespace Comgate\SDK\Logging;
 
 use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
+use Stringable;
 
 class StdoutLogger implements LoggerInterface
 {
@@ -18,43 +19,66 @@ class StdoutLogger implements LoggerInterface
 		echo "[{$level}]: {$message}";
 	}
 
-
-	public function emergency($message, array $context = array())
+	/**
+	 * @param mixed|string|Stringable $param
+	 */
+	public function emergency($message, array $context = []): void
 	{
 		$this->log(LogLevel::EMERGENCY, $message, $context);
 	}
 
-	public function alert($message, array $context = array())
+	/**
+	 * @param mixed|string|Stringable $param
+	 */
+	public function alert($message, array $context = []): void
 	{
 		$this->log(LogLevel::ALERT, $message, $context);
 	}
 
-	public function critical($message, array $context = array())
+	/**
+	 * @param mixed|string|Stringable $param
+	 */
+	public function critical($message, array $context = []): void
 	{
 		$this->log(LogLevel::CRITICAL, $message, $context);
 	}
 
-	public function error($message, array $context = array())
+	/**
+	 * @param mixed|string|Stringable $param
+	 */
+	public function error($message, array $context = []): void
 	{
 		$this->log(LogLevel::ERROR, $message, $context);
 	}
 
-	public function warning($message, array $context = array())
+	/**
+	 * @param mixed|string|Stringable $param
+	 */
+	public function warning($message, array $context = []): void
 	{
 		$this->log(LogLevel::WARNING, $message, $context);
 	}
 
-	public function notice($message, array $context = array())
+	/**
+	 * @param mixed|string|Stringable $param
+	 */
+	public function notice($message, array $context = []): void
 	{
 		$this->log(LogLevel::NOTICE, $message, $context);
 	}
 
-	public function info($message, array $context = array())
+	/**
+	 * @param mixed|string|Stringable $param
+	 */
+	public function info($message, array $context = []): void
 	{
 		$this->log(LogLevel::INFO, $message, $context);
 	}
 
-	public function debug($message, array $context = array())
+	/**
+	 * @param mixed|string|Stringable $param
+	 */
+	public function debug($message, array $context = []): void
 	{
 		$this->log(LogLevel::DEBUG, $message, $context);
 	}
