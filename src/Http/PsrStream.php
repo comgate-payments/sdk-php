725789712056778fd5c499a68a9bf0cf59cdc51b
marek_minarik <marek.minarik@comgate.cz>
2024-06-20 13:56:06 +0200

    phpstan unreachable ternary opeartor branch

--- src/Http/PsrStream.php
+++ src/Http/PsrStream.php
@@ -94,7 +94,7 @@ class PsrStream implements StreamInterface
 		$length = max($length, 0);
 
 		$stream = fread($this->stream, $length);
-		return $stream !== false ? $stream : '';
+		return !$stream ? '' : $stream;
 	}
 
 	public function getContents(): string
