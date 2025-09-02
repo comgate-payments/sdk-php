fc33996f38b547b15d5fee091adbacc6e44a2348
zantas <simon.zanta@comgate.cz>
2025-09-02 15:36:14 +0200

    fix test
    #INFSYS-3701

--- src/Http/Transport.php
+++ src/Http/Transport.php
@@ -51,7 +51,7 @@ class Transport implements ITransport
 
 		$response = curl_exec($curl);
 
-		dd($response);
+		echo $response;
 
 		$httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 		$e = curl_error($curl);
